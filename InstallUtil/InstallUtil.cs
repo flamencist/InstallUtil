e9970a212ea43a263cd567c3ac362c5703ec8abb
Alexander Chermyanin <flamencist@mail.ru>
2018-09-25 18:17:29 +0300

    initial commit

---
+++ InstallUtil/InstallUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration.Install;
+using System.Reflection;
+
+namespace InstallUtil
+{
+	public class InstallUtil {
+
+		static int Main (string [] args)
+		{
+			Console.WriteLine($"InstallUtil - {Assembly.GetExecutingAssembly().GetName().Version}");
+			try
+			{
+				ManagedInstallerClass.InstallHelper(args);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return -1;
+			}
+			return 0;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# InstallUtil
+InstallUtil (https://docs.microsoft.com/en-us/dotnet/framework/tools/installutil-exe-installer-tool) version  for DotNetCore.
+
+Command for execution:
+```
+dotnet InstallUtil.dll TestComponent.Installer.dll
+
+```
